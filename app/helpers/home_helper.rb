cfa4054e4b5914be4332d1429ba4e5787f7caabf
Corinna Wiesner <wiesner@avarteq.de>
2015-10-07 07:35:28 +0200

    fix registration form links

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,9 +2,9 @@ module HomeHelper
 
   def sign_up_link
     if I18n.locale == :en
-      'https://railsgirlssb.wufoo.com/forms/apply-for-railsgirls-saarbracken-67-november-2015/'
+      'https://railsgirlssb.wufoo.com/forms/apply-for-railsgirls-saarbracken-7th-november-2015/'
     else
-      'https://railsgirlssb.wufoo.com/forms/anmeldung-railsgirls-saarbracken-67-november-2015/'
+      'https://railsgirlssb.wufoo.com/forms/anmeldung-railsgirls-saarbracken-7-november-2015/'
     end
   end
 
